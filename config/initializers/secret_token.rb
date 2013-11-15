b96e1f9a47b2f2d492d49bd6de53a96ee729111c
JAdshead <jonny.adshead@gmail.com>
2013-11-15 13:23:58 +0000

    created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoApp::Application.config.secret_token = '29f0a5a4c6f94beade8fd02902f21fbdd4ae810f05d655f125dae5af0b806d0fcce231517eb80f5355ed5839b847ae244e0ac4350d9c68c6f8dfa911f3616095'
