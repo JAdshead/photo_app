19e828f954aa2da6529f61002546e5f16e89c884
JAdshead <jonny.adshead@gmail.com>
2013-11-16 19:52:21 +0000

    doing fixes from merge

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,4 +1,5 @@
 FactoryGirl.define do
+
   factory :user do
     sequence(:name) { |n| "user-#{n}" } #sequence
     email { "#{name}@gmail.com" }       #dependent attribute
@@ -9,6 +10,7 @@ FactoryGirl.define do
       ignore do
         album_count 3 # tells FG this is NOT an attribute
       end
+    end
 
     after(:create) do |user, evaluator|
       FactoryGirl.create_list :album, evaluator.album_count, user: user
