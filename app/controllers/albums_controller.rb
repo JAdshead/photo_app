d397a9364d160207440edfe9d28c8354d44bcdcb
JAdshead <jonny.adshead@gmail.com>
2013-11-18 17:26:59 +0000

    tidy up pictures

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -17,7 +17,7 @@ before_filter :authenticate_user!, except: [:show, :index]
   end
 
   def create
-    @album = Album.new 
+    @album = Album.new params[:album]
     @album.user_id = current_user.id
     @album.save
     redirect_to @album
