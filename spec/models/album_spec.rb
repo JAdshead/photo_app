8dd4fed59a67303aa5ebee0f39853751ccd23bb4
SelinaC <selina.chotai@gmail.com>
2013-11-18 16:42:47 +0000

    correcting specs for changes to user model

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,7 +2,6 @@ require "spec_helper"
 
 describe Album do
   describe "validations" do
-    it{ should validate_presence_of :user_id }
     it{ should validate_presence_of :title }
     it{ should validate_presence_of :description}
   end
@@ -10,7 +9,6 @@ describe Album do
   # uniqueness of user id
 
   describe "mass assignment" do
-    it{ should allow_mass_assignment_of :user_id}
     it{ should allow_mass_assignment_of :title}
     it{ should allow_mass_assignment_of :description}
   end
@@ -19,6 +17,7 @@ describe Album do
     let!(:album){ FactoryGirl.create :album_with_photos, photo_count: 3 }
     it "has the correct photo count" do
       album.photos.count.should == 3
+    end
   end
 
 end
