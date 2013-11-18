8e0453570b05f9048179daceb7f8cd4ac4656cde
JAdshead <jonny.adshead@gmail.com>
2013-11-18 14:17:26 +0000

    initial commit

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,45 @@
 class AlbumsController < ApplicationController
 
+before_filter :authenticate_user!, except: [:show, :index]
+
+  def index
+    @albums = Album.all
+  end
+
+  def show
+    @photo = Photo.new
+    @album = Album.find params[:id]
+  end
+
+  def new
+    @album = Album.new(:user_id => params[:user_id])
+  end
+
+  def create
+    @album = Album.create params[:album]
+    redirect_to @album
+  end
+
+  def edit
+    @album = Album.find params[:id]
+  end
+
+  def update
+    album = Album.find params[:id]
+    album.update_attributes params[:album]
+    redirect_to album
+  end
+
+  def destroy
+    album = Album.find params[:id]
+    user = User.find(album.user_id)
+    album.delete
+    redirect_to user
+  end
+
+end
+class AlbumsController < ApplicationController
+
   def index
     @albums = Album.all
   end
