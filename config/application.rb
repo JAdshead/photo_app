00dbc4f53c457065763b75a47e948459b4026a80
JAdshead <jonny.adshead@gmail.com>
2013-11-19 22:23:56 +0000

    fix heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ end
 
 module PhotoApp
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -59,6 +60,9 @@ module PhotoApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
+
 
     config.generators do |g|
       g.test_framework :rspec,
