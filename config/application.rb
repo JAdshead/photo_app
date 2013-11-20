404100164c81336473707410051d37aeb487d363
JAdshead <jonny.adshead@gmail.com>
2013-11-20 22:59:23 +0000

    asdf

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ end
 
 module PhotoApp
   class Application < Rails::Application
+    
     config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -60,9 +61,6 @@ module PhotoApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-
-
 
     config.generators do |g|
       g.test_framework :rspec,
