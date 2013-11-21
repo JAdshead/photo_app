6a4aa16f3b0b56bc63f6e449667fdf052b654d7a
jacklalley <jckly13@gmail.com>
2013-11-21 23:13:53 +0000

    adjusted the forms

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,7 +13,7 @@ $(function() {
     $(this).hide();
     $( ".profile-grid-photo.add form" ).fadeIn(200);
     $(".profile-grid-photo.add form").fadeIn(500);
-    $(".profile-grid-photo.add").animate({height: "29em"}, 500);
+    $(".profile-grid-photo.add").animate({height: "29em"}, 400);
   });
 
 });
