fba7e4ad27d2973215f4f4fd1a6471f01671fa7c
JAdshead <jonny.adshead@gmail.com>
2013-11-21 19:08:26 +0000

    style fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
 
   def index
     page = params[:page] || 1
-    per_page = 6
+    per_page = 9
     @photos = Photo.plusminus_tally.order('plusminus_tally DESC').paginate(page: page, per_page: per_page)
   end
 
